68f3fe00f17171fd1ef8d0f74e5473fe1d491376
jade <jadeiskid1@gmail.com>
2021-03-22 11:00:47 -0400

    01 Tri de boules

--- tasks/11_recursion.rb
+++ tasks/11_recursion.rb
@@ -1,49 +1,24 @@
 
-$graphical = true
-require_relative "../karel/robota"
-require_relative "../karel/ur_robot"
-
-class Recursion11 < UrRobot
-	def move_until
-		while front_is_clear?
-			move
-		end
+def move_until
+	while front_is_clear?
+		move		
 	end
+end
 	
-	def move_until
-		if front_is_clear?
-			move
-			move_until
-		end
-	end
-
-	def put_beeper(n)
-		unless (any_beepers_in_beeper_bag?==0)
-			super
-			put_beeper(n)
-		end
-	end
-
-	def spirale
-
+def move_until
+	if front_is_clear?
+		move
+		move_until
 	end
 end
 
-def task()
-	world = Robota::World
-	world.read_world("../worlds/aleatoire.kwld")
-
-	karel = T8Q3.new(2, 2, Robota::North, 0)
-	karel.clear_world
+def put_beeper(n)
+	unless (any_beepers_in_beeper_bag?==0)
+		super
+		put_beeper(n)
+	end
 end
 
-if __FILE__ == $0
-	if $graphical
-		screen = window(12, 40)
-		screen.run do
-		task
-	end
-	else
-		task
-	end
+def spirale
+
 end
